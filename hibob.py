aead810feeb13c411c6c65f6e327569884bf6fc8
Jordan Scott <scotter567@gmail.com>
2017-05-01 20:23:59 -0400

    added bit by bit

--- hibob.py
+++ hibob.py
@@ -1,4 +1,10 @@
 inputs = ['2041004C', '2062002B', '00221820', '00412822', 'AD010007', '8D090007']
+
+registers = ['00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000',
+          '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000',
+          '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000',
+          '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000']
+
 memory = ['00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000',
           '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000',
           '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000',
@@ -22,7 +28,113 @@ def hex_binary(inputs):
         new_val = ''
     return binary_list
 
+def r_get_command_line(y):
+    op = int ((y[:6]), 2)
+    rs = int ((y[6:11]), 2)
+    rt = int ((y[11:16]), 2)
+    rd = int ((y[16:21]), 2)
+    shift = int ((y[21:26]), 2)
+    funon = int ((y[26:]), 2)
+
+    return op, rs, rt, rd, shift, funon
+
+def i_get_command_line(y):
+    op = int (y[:6], 2)
+    rs = int (y[6:11], 2)
+    rt = int (y[11:16], 2)
+    Im = int (y[16:] , 2)
+
+    return op, rs, rt, Im
+
+def bit_by_bit(x,y):
+#AND
+    for i in range(len(x), 0, -1):
+        x1 = x[i-1]
+        y1 = y[i-1]
+
+        if x1 == '1' and y1 == '1':
+            print (1, end = '')
+        else:
+            print (0, end = '')
+
+    print ()
+
+#OR
+    for i in range(len(x), 0, -1):
+        x1 = x[i - 1]
+        y1 = y[i - 1]
+
+        if x1 == '0' and y1 == '0':
+            print (0, end = '')
+        else:
+            print (1, end = '')
+
+    print ()
+    print ()
+#ADD
+    carry = 0
+    new_binary = ''
+    print (x)
+    print (y)
+    print()
+    for i in range(len(x), 0, -1):
+        x1 = (x[i - 1])
+        y1 = (y[i - 1])
+
+        if x1 == '1' and y1 == '1':
+            if carry == 1:
+                new_binary = '1' + new_binary
+                carry = 1
+            else:
+                new_binary = '0' + new_binary
+                carry = 0
+
+        elif x1 == '1' and y1 == '0':
+            if carry == 1:
+                new_binary = '0' + new_binary
+                carry = 0
+            else:
+                new_binary = '1' + new_binary
+                carry = 0
+
+        elif x1 == '0' and y1 == '1':
+            if carry == 1:
+                new_binary = '0' + new_binary
+                carry = 0
+            else:
+                new_binary = '1' + new_binary
+                carry = 0
+
+        else:
+            if carry == 1:
+                new_binary = '1' + new_binary
+                carry = 0
+            else:
+                new_binary = '0' + new_binary
+                carry = 0
+        print (new_binary, "Carry is: ",carry)
+#        print(carry)
+#        print()
+    return new_binary
+
+
+
 binary_list = hex_binary(inputs)
 
+#for i in range(len(binary_list)):
+print (binary_list[0])
+print (binary_list[1])
+print ()
+
+command_list = []
+
 for i in range(len(binary_list)):
-    print (binary_list[i])
+    y = binary_list[i]
+    if (int(y[:6], 2)) == 0:
+        command_list.append((r_get_command_line(y)))
+
+    else:
+        command_list.append((i_get_command_line(y)))
+
+z = command_list[2]
+print (bit_by_bit(binary_list[0], binary_list[1]))
